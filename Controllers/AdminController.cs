0397bc46c4a4c681d0ed8eb18ddff2ac9ced8a6c
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-29 14:05:31 -0600

    fix:  se agrgean nuevos elementos de desarrollo para el controlador de admin, con la finalidad de agregar roles y eliminar roles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -23,7 +23,24 @@ namespace TiendaEcomerce.Controllers
             var users = _userManager.Users.ToList();
             return View(users);
         }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+            if (!await _roleManager.RoleExistsAsync(role)) await _roleManager.CreateAsync(new IdentityRole(role));
+            await _userManager.AddToRoleAsync(user, role);
+            return RedirectToAction("Users");
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> RemoveRole(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+            await _userManager.RemoveFromRoleAsync(user, role);
+            return RedirectToAction("Users");
+        }
         public IActionResult Index()
         {
             return View();
