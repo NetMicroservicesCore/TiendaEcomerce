0afc4479348b7f502736835ea3b3348205e17c2a
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-29 14:03:55 -0600

    feat: se agrega el metodo necesario para trabajar con la administración del controlador , usuarios y roles.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TiendaEcomerce.Models;
+
+namespace TiendaEcomerce.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Users()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
