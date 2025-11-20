4fa2e763ac29e19d9f53798f7d4e85dac1e4afed
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-11-20 13:43:22 -0600

    feat: agregamos una propuesta de la vista  para asiganr los roles a loas permisos y viceversa,  configurando el prodesos de  asignacion de los roles a la seguridad del  sistema

---
+++ Models/PermissionSelection.cs
@@ -0,0 +1,10 @@
+﻿namespace TiendaEcomerce.Models
+{
+    public class PermissionSelection
+    {
+        public int PermissionId { get; set; }
+        public string? Key { get; set; }
+        public string? Description { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
---
+++ Models/RolePermissionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TiendaEcomerce.Models
+{
+    public class RolePermissionViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public List<PermissionSelection> Permissions { get; set; }
+    }
+}
